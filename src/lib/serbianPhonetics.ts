0054ce96fb544aaf2975199529b7431f77760522
Efstratios Demertzoglou <95057772+StratosDns@users.noreply.github.com>
2026-03-02 00:35:35 +0200

    Update phonetic mapping for 'nja' to 'nyah'

--- src/lib/serbianPhonetics.ts
+++ src/lib/serbianPhonetics.ts
@@ -46,7 +46,7 @@ const phoneticMap: [string, string][] = [
   ["veće", "vetcheh"], ["Veće", "Vetcheh"], ["VEČE", "VECTHEH"],
   ["ete ", "et"], ["ETE ", "ET"],
   ["noć", "nots"], ["Noć", "Nots"], ["NOĆ", "NOTS"],
-  ["nja", "nya"], ["Nja", "Nya"], ["NJA", "NYA"],
+  ["nja", "nyah"], ["Nja", "Nyah"], ["NJA", "NYAH"],
   ["ćao", "tchaoh"], ["Ćao", "Tchaoh"], ["ĆAO", "TCHAOH"],
   ["lj", "lyuh"], ["Lj", "Lyuh"], ["LJ", "LYUH"],
   ["nj", "ny"],   ["Nj", "Ny"],   ["NJ", "NY"],
